c6257b805d9de535827e4888088bbe3dc59f0ea6
colbyfayock <fayock@gmail.com>
2021-10-18 22:31:23 -0400

    removing search

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -1,8 +1,6 @@
-import { useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link';
-import Fuse from 'fuse.js'
 
 import Layout from '@components/Layout';
 import Container from '@components/Container';
@@ -13,27 +11,6 @@ import products from '@data/products.json';
 import styles from '@styles/Home.module.scss'
 
 export default function Home() {
-  const [query, setQuery] = useState();
-
-  let activeProducts = products;
-
-  const fuse = new Fuse(activeProducts, {
-    keys: [
-      'title',
-      'allegiance'
-    ]
-  });
-
-  if ( query ) {
-    const results = fuse.search(query);
-    activeProducts = results.map(({ item }) => item);
-  }
-
-  function handleOnSearch(event) {
-    const value = event.currentTarget.value;
-    setQuery(value);
-  }
-
   return (
     <Layout>
       <Head>
@@ -44,18 +21,9 @@ export default function Home() {
       <Container>
         <h1 className="sr-only">Hyper Bros. Trading Cards</h1>
 
-        <div className={styles.discover}>
-          <div className={styles.search}>
-            <h2>Search</h2>
-            <form>
-              <input onChange={handleOnSearch} type="search" />
-            </form>
-          </div>
-        </div>
-
         <h2 className="sr-only">Available Cards</h2>
         <ul className={styles.products}>
-          {activeProducts.map(product => {
+          {products.map(product => {
             return (
               <li key={product.id}>
                 <Link href={`/products/${product.id}`}>
